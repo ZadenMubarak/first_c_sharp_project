434997849858330d55690a1dda3cdeeb8c00359a
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-08-01 23:19:02 +0200

    set up the first class

---
+++ HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+
+namespace HelloWorld
+{
+
+	public class Program
+	{
+		public Program()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
