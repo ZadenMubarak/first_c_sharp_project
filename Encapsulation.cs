81b275913aa81ba9c9feef37d535d4ea72faf0bd
ZadenMubarak <zaden.ngobeni@gmail.com>
2025-09-03 00:16:06 +0200

    Adding oop concepts

---
+++ Encapsulation.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Encapsulation
+{
+    public class  program 
+    {
+
+        private int age;
+        private String name;
+        private String occupation;
+
+        public void setAge(int age) { }
+        public int getAge() { }
+        
+    }
+}
